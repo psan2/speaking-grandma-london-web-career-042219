5f9abf3b02750249f5345a71375dc5956645e58f
Patrick Santucci <patrick.s.santucci@gmail.com>
2019-03-14 14:30:45 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -14,7 +14,7 @@
 def speak_to_grandma(say)
   if(say=="I LOVE YOU GRANDMA!")
     "I LOVE YOU TOO PUMPKIN!"
-  elsif(say.upper==say)
+  elsif(say.capitalize==say)
     "NO, NOT SINCE 1938!"
   else
     "HUH?! SPEAK UP, SONNY!"
